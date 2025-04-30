5a7f6f15f139889116494b0aca6a351a94b8bcf4
PinkieZH <elenacolletpro3001@gmail.com>
2025-04-30 20:38:09 +0200

    je commit ça pr l'instant (enemybehaviour pas fini du tt je fais sur un autre script pr tt tester j'ai pas inclu le script psk jvais le suppr)

--- Assets/Scripts/Enemies/EnemyBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -1,24 +1,89 @@
+using UnityEditor.VersionControl;
 using UnityEngine;
+
 
 namespace Enemies
 {
     public class EnemyBehaviour : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [Header("Detection Settings")]
+        public Transform cubeTransform;
+        public float detectionRadius = 4f;
+        public float chaseSpeed = 2.5f;
+        public float chaseTime = 4f;
+        public LayerMask obstacleLayer;
+        public Rigidbody2D rb;
+        public Animator anim;
+        public bool isFound;
+        public float rangeBat = 2;
+        public GameObject cube;
+
+        public enum EnemyState
         {
-        
+            Search,
+            Attack,
+            Cooldown
         }
 
-        // Update is called once per frame
+        private EnemyState currentState;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            currentState = EnemyState.Search;
+            isFound = false;
+        }
+
         void Update()
         {
-        
+            switch (currentState)
+            {
+                case EnemyState.Search:
+                    Search();
+                    break;
+                case EnemyState.Attack:
+                    Attack();
+                    break;
+                case EnemyState.Cooldown:
+                    Cooldown();
+                    break;
+            }
+        }
+
+        public void Search()
+        {
+            if (isFound == false)
+            {
+                Collider2D cubeCollider = Physics2D.OverlapCircle(transform.position, rangeBat);
+                if (cubeCollider.gameObject.CompareTag("Cube"))
+                {
+                    isFound = true;
+                    cube = cubeCollider.gameObject;
+                    currentState = EnemyState.Attack;
+                }
+            }
+        }
+
+        public void Attack()
+        {
+            //dotween nik ta mere la pute
+            //currentState = EnemyState.Cooldown
+        }
+
+        public void Cooldown()
+        {
+
+        }
+
+        public void Died()
+        {
+            gameObject.SetActive(false);
         }
 
         public void TakeDamage()
         {
 
         }
+
     }
 }
