538fd960b4567287a94dd9c80692effd04aa7d3f
PinkieZH <elenacolletpro3001@gmail.com>
2025-05-05 02:53:05 +0200

    enemy behavior (attack, cooldown)
    j'ai fais le dotween mais j'arrive pas à trouver pourquoi l'ennemi n'attaque plus même après le cooldown alors qu'il est tjr dans le rayon pour attaquer le cube genre le search ne marche pas s'il est déjà dans le rayon du cube faut genre que le cube parte et revienne pr qu'il le redétecte ??

--- Assets/Scripts/Enemies/EnemyBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -1,84 +1,107 @@
 using UnityEngine;
+using DG.Tweening;
 
-    public class EnemyBehaviour : MonoBehaviour
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [Header("Detection Settings")]
+    private Transform cubeTransform;
+    [SerializeField] private float detectionRadius = 4f;
+    [SerializeField] private float chaseSpeed = 2.5f;
+    [SerializeField] private float chaseTime = 4f;
+    private LayerMask obstacleLayer;
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private bool isFound;
+    [SerializeField] private float rangeBat = 2;
+    private GameObject cube;
+
+
+
+
+    [Header("Cooldown Settings")]
+    [SerializeField] private float attackCooldownDuration = 5f;
+    private float cooldownTimer;
+
+    private enum EnemyState
     {
-        [Header("Detection Settings")]
-        private Transform cubeTransform;
-        [SerializeField] private float detectionRadius = 4f;
-        [SerializeField] private float chaseSpeed = 2.5f;
-        [SerializeField] private float chaseTime = 4f;
-        private LayerMask obstacleLayer;
-        private Rigidbody2D rb;
-        private Animator anim;
-        [SerializeField] private bool isFound;
-        [SerializeField] private float rangeBat = 2;
-        private GameObject cube;
+        Search,
+        Attack,
+        Cooldown
+    }
 
-        private enum EnemyState
+    private EnemyState currentState;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentState = EnemyState.Search;
+        isFound = false;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
         {
-            Search,
-            Attack,
-            Cooldown
+            case EnemyState.Search:
+                Search();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Cooldown:
+                Cooldown();
+                break;
         }
+    }
 
-        private EnemyState currentState;
+    private void Search()
+    {
+        Collider2D cubeCollider = Physics2D.OverlapCircle(transform.position, rangeBat);
 
-        private void Awake()
+        if (isFound == false)
         {
-            rb = GetComponent<Rigidbody2D>();
+            if (cubeCollider.gameObject.CompareTag("Cube"))
+            {
+                isFound = true;
+                cube = cubeCollider.gameObject;
+                currentState = EnemyState.Attack;
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        Vector3 targetPos = cube.transform.position;
+
+        transform.DOMove(targetPos, 0.5f)
+        .SetLoops(2, LoopType.Yoyo)
+        .SetEase(Ease.InOutSine)
+        .OnComplete(() =>
+        {
+            cooldownTimer = attackCooldownDuration;
+            currentState = EnemyState.Cooldown;
+        });
+    }
+
+    private void Cooldown()
+    {
+        isFound = false;
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0f)
+        {
             currentState = EnemyState.Search;
-            isFound = false;
         }
+    }
 
-        private void Update()
-        {
-            switch (currentState)
-            {
-                case EnemyState.Search:
-                    Search();
-                    break;
-                case EnemyState.Attack:
-                    Attack();
-                    break;
-                case EnemyState.Cooldown:
-                    Cooldown();
-                    break;
-            }
-        }
+    public void Died()
+    {
+        gameObject.SetActive(false);
+    }
 
-        private void Search()
-        {
-            if (isFound == false)
-            {
-                Collider2D cubeCollider = Physics2D.OverlapCircle(transform.position, rangeBat);
-                if (cubeCollider.gameObject.CompareTag("Cube"))
-                {
-                    isFound = true;
-                    cube = cubeCollider.gameObject;
-                    currentState = EnemyState.Attack;
-                }
-            }
-        }
-
-        private void Attack()
-        {
-            //dotween nik ta mere la pute
-            //currentState = EnemyState.Cooldown
-        }
-
-        private void Cooldown()
-        {
-
-        }
-
-        public void Died()
-        {
-            gameObject.SetActive(false);
-        }
-
-        public void TakeDamage()
-        {
-
-        }
+    public void TakeDamage()
+    {
 
     }
+
+}
